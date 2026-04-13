b96dfecc155b17b3dc8e28123d2c36720c15796b
RahafAl0 <ra.al9760@gmail.com>
2026-04-13 18:52:03 +0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import { rules } from "@eslint/js/src/configs/eslint-all";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat.recommended,
+  {
+    files: ["**/*.js,**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/no-unscqped-entities": "off",
+      "react/prop-types": "off",
+    },
+  },
+  prettier,
+];
